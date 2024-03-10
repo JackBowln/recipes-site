569df779ca95d3f70a1e3cdc83068f4ad6d7d67f
Vinicius Viola Dias <vinivdias@gmail.com>
2024-03-10 13:13:10 +0000

    Add recipes data

---
+++ src/data/recipes.js
@@ -0,0 +1,55 @@
+export const cookbook = {
+  recipes: [
+      {
+          name: "Lasanha à Bolonhesa",
+          ingredients: ["macarrão para lasanha", "carne moída", "molho de tomate", "queijo ralado", "cebola", "alho", "azeite", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Risoto de Cogumelos",
+          ingredients: ["arroz arbóreo", "cogumelos", "caldo de legumes", "vinho branco", "cebola", "manteiga", "queijo parmesão", "sal", "pimenta"],
+          image: "https://www.pingodoce.pt/wp-content/uploads/2021/07/risotto-de-cogumelos-com-caldo-de-espargos.jpeg"
+      },
+      {
+          name: "Frango Assado com Batatas",
+          ingredients: ["peito de frango", "batatas", "limão", "alho", "alecrim", "azeite", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+
+      },
+      {
+          name: "Salada Caesar",
+          ingredients: ["alface romana", "croutons", "queijo parmesão", "peito de frango grelhado", "molho caesar", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Sopa de Legumes",
+          ingredients: ["cenoura", "batata", "abobrinha", "cebola", "alho", "caldo de legumes", "salsinha", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Penne ao Molho Pesto",
+          ingredients: ["penne", "manjericão", "azeite", "queijo parmesão", "alho", "pinhões", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Torta de Maçã",
+          ingredients: ["maçãs", "açúcar", "canela", "farinha de trigo", "manteiga", "água", "limão", "sal"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Hambúrguer Caseiro",
+          ingredients: ["carne moída", "pão de hambúrguer", "queijo cheddar", "alface", "tomate", "cebola", "molho de ketchup", "molho de mostarda", "sal", "pimenta"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Tiramisù",
+          ingredients: ["queijo mascarpone", "ovos", "açúcar", "café forte", "licor de café", "biscoitos tipo ladyfinger", "cacau em pó"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      },
+      {
+          name: "Sushi",
+          ingredients: ["arroz para sushi", "peixe fresco (salmão, atum, etc.)", "alga nori", "vinagre de arroz", "açúcar", "sal", "wasabi", "molho de soja"],
+          image: "https://static.itdg.com.br/images/360-240/ec2a5e38702c60bf1ace0b5f1c8e9415/shutterstock-739787011.jpg"
+      }
+  ]
+};
