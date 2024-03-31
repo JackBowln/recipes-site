2c4a3e8a903514b7e6e1db71cae7c590182174dd
Dinotopico <deividdias11@outlook.com>
2024-03-31 17:42:50 +0100

    Change router

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import Search from './components/Search.vue'
-import Cards from './components/Cards.vue'
-import { cookbook } from './data/recipes.js'
-import { ref } from 'vue'
-import axios from 'axios'
-import {onMounted } from 'vue'
-import { supabase } from './api/supabaseClient'
-
-  const recipes = ref([])
-
-  async function getRecipes() {
-    const { data } = await supabase.from('recipes').select()
-    recipes.value = data
-  }
-    getRecipes()
-
-
-const information = 'Pesquise...'
 </script>
 
 <template>
-  <div class="d-flex flex-column">
-    <header class="d-flex justify-center">
-      <Search :information :search-content v-model="searchContent" />
-    </header>
-    <Cards
-      :loading="false"
-      v-for="(value, index) in recipes"
-      :recipe="value"
-      :key="index"
-    />
-  </div>
+  <nav>
+    <RouterLink :to="{ name: 'receita' }">Home</RouterLink>
+    <br />
+    <RouterLink :to="{ name: 'registo' }">Registo</RouterLink>
+  </nav>
 
-  <!--<RouterView />-->
+  <RouterView />
 </template>
 
 <style scoped></style>
